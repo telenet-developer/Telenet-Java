c58ddd1971f873d9e518d66fe7d0c564c5810741
RAHEMAN-PC <RAHEMAN-PC@RAHEMAN>
2020-09-24 23:09:38 +0530

    [TECH][STORY-1A],[TECH][STORY-1B]

---
+++ src/main/java/com/telenet/movie/model/Movie.java
@@ -0,0 +1,33 @@
+package com.telenet.movie.model;
+
+public class Movie {
+
+	private String title;
+	private String description;
+	private String releaseDate;
+	private String genere;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public String getGenere() {
+		return genere;
+	}
+	public void setGenere(String genere) {
+		this.genere = genere;
+	}
+}
